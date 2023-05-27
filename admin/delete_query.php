bc02a12afe6ebd74536f8423e671cb6b39beb0d0
nandishTech01 <nadishbhuva5@gmail.com>
2023-05-27 21:58:21 +0530

    first changes

---
+++ admin/delete_query.php
@@ -0,0 +1,15 @@
+<?php
+include 'conn.php';
+
+$que_id = $_GET['id'];
+$sql= "DELETE FROM contact_query where query_id='{$que_id}'";
+$result=mysqli_query($conn,$sql);
+mysqli_close($conn);
+
+ ?>
+
+ <?php
+
+ header ("Location: http://localhost/BDMS/admin/query.php ");
+ exit();
+ ?>
